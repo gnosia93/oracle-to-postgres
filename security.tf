2d7cf890f300b24c8652c75ddc0ad3b2ce1d7148
Kwon <soonbeom@f8ffc2077dc2.ant.amazon.com>
2021-01-28 22:22:50 +0900

    ..

--- security.tf
+++ security.tf
@@ -68,6 +68,48 @@ EOF
 
 
 
+
+## for dms pre migration assessment
+resource "aws_iam_role" "tf_dms_service_role" {
+  name = "tf_dms_service_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "dms.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "tf_dms_service_role_policy" {
+  name = "tf_dms_service_role_policy"
+  role = aws_iam_role.tf_dms_service_role.id
+  policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
+    {
+        "Effect": "Allow",
+        "Action": [
+            "s3:*"           
+        ],
+        "Resource": "*"
+    }
+]
+}
+EOF
+}
+
+
+
 resource "aws_iam_role" "tf_ec2_service_role" {
   name = "tf_ec2_service_role"
   assume_role_policy = <<EOF
