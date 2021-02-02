3ef9629cc561ade30fb7c67b447d6c316ac6010c
Kwon <soonbeom@f8ffc2077dc2.ant.amazon.com>
2021-02-02 20:31:32 +0900

    ..

--- dms.tf
+++ dms.tf
@@ -2,7 +2,7 @@ resource "null_resource" "previous" {}
 
 resource "time_sleep" "wait_10_seconds" {
   depends_on = [null_resource.previous]
-  create_duration = "10s"
+  create_duration = "30s"
 }
 
 
