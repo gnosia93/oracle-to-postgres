bd0a1f8debe9db80edcd4aa67fdd92de6079b7aa
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2021-08-08 07:53:53 +0900

    Update ec2.tf

--- tf/ec2.tf
+++ tf/ec2.tf
@@ -18,13 +18,13 @@ sudo yum install -y python37 git telnet
 sudo pip3 install cx_oracle
 sudo -u ec2-user git clone https://github.com/gnosia93/postgres-pyoracle.git /home/ec2-user/pyoracle
 sudo -u ec2-user mkdir -p /home/ec2-user/pyoracle/images
-sudo -u ec2-user curl -o /home/ec2-user/pyoracle/images/images.tar https://demo-database-postgres.s3.ap-northeast-2.amazonaws.com/images/images.tar
+sudo -u ec2-user curl -o /home/ec2-user/pyoracle/images/images.tar http://ec2-3-37-255-59.ap-northeast-2.compute.amazonaws.com/images.tar
 sudo -u ec2-user tar xvf /home/ec2-user/pyoracle/images/images.tar -C /home/ec2-user/pyoracle/images
-sudo -u ec2-user curl -o /home/ec2-user/client.zip https://demo-database-postgres.s3.ap-northeast-2.amazonaws.com/instantclient-basic-linux.x64-21.1.0.0.0.zip
+sudo -u ec2-user curl -o /home/ec2-user/client.zip http://ec2-3-37-255-59.ap-northeast-2.compute.amazonaws.com/instantclient-basic-linux.x64-21.1.0.0.0.zip
 sudo -u ec2-user mkdir -p /home/ec2-user/oracle
 sudo -u ec2-user unzip /home/ec2-user/client.zip -d /home/ec2-user/oracle
 sudo -u ec2-user mv /home/ec2-user/oracle/instantclient_21_1 /home/ec2-user/oracle/lib
-sudo -u ec2-user curl -o /home/ec2-user/sqlplus.zip https://demo-database-postgres.s3.ap-northeast-2.amazonaws.com/instantclient-sqlplus-linux.x64-21.1.0.0.0.zip
+sudo -u ec2-user curl -o /home/ec2-user/sqlplus.zip http://ec2-3-37-255-59.ap-northeast-2.compute.amazonaws.com/instantclient-sqlplus-linux.x64-21.1.0.0.0.zip
 sudo -u ec2-user unzip /home/ec2-user/sqlplus.zip -d /home/ec2-user/oracle/lib
 sudo -u ec2-user mv /home/ec2-user/oracle/lib/instantclient_21_1/*.so /home/ec2-user/oracle/lib
 sudo -u ec2-user mv /home/ec2-user/oracle/lib/instantclient_21_1/sqlplus /home/ec2-user/oracle
